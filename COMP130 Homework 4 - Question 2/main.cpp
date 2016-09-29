e5487248e11feca2e98d8bbc35013992a129f8fc
alexberry721 <alexberry721@gmail.com>
2016-09-28 22:33:31 -0700

    Welp, good enug

--- COMP130 Homework 4 - Question 2/main.cpp
+++ COMP130 Homework 4 - Question 2/main.cpp
@@ -6,10 +6,33 @@
 //  Copyright © 2016 COMP130. All rights reserved.
 //
 
-#include <iostream>
+#include<iostream>
+#include<string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    string str_input = " ";
+    
+    cout << "Enter a string of some length: ";
+    
+    // enter ssn
+    getline(cin, str_input);
+    
+    int word_count = 1;
+    
+    for (int i = 0; i <= str_input.length(); i++) {
+        if (str_input[i] == ' ') {
+            // if the space has a space ahead of it,
+            if (str_input[i + 1] == ' ') {
+                continue; // continue to count just one word per spaces between words
+            } else {
+                word_count++;
+            }
+        }
+    }
+    
+    cout << "The string has " << word_count << " words" << endl;
+    
     return 0;
 }
